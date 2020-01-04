7d8a8f03c6780b7ff270ee9b2bc34f4a192db99d
erees <erees@protonmail.com>
2020-01-04 13:26:29 -0500

    Building out form functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
